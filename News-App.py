645c420d5a8b66aa806b18af80ba1d3cc9d96302
vikramx <30879085+vikramx@users.noreply.github.com>
2025-04-27 14:26:42 +0530

    Update News-App.py

--- News-App.py
+++ News-App.py
@@ -7,25 +7,29 @@ st.subheader("An app to show you the news for the day")
 
 
 topic=st.text_input("Enter news topic here:")
-ps=st.number_input("Enter number of articles in 1 page here:")
+ps=st.number_input("Enter number of articles in 1 page here:",min_value=1)
 api_key="d6294e9b2ca842c1815660bfdcfabd42"
 base_url=(f"https://newsapi.org/v2/everything?q={topic}&apiKey=d6294e9b2ca842c1815660bfdcfabd42")
 p={
     "pageSize":ps
 }
-st.write(base_url)
 response=requests.get(base_url,params=p)
 data=response.json()
 if st.button("Show articles"):
-    art_list = data['articles']
-    num = 1
-    st.write("Headlines")
-    for i in art_list:
-        st.write(f"{num}: Headlines {num}\n")
-        st.write(f"Title: {i['title']}")
-        st.write(f"Author: {i["author"]}")
-        st.write(f"News: {i['description']}")
-        st.write(f"Link to the article: {i["url"]}")
-        st.write('---------------------------------')
+    if response.status_code==200 and data["totalResults"] != 0:
+        art_list = data['articles']
+        num = 1
+        st.write("Headlines")
+        for i in art_list:
+            st.write(f"{num}: Headlines {num}\n")
+            img = i["urlToImage"]
+            st.image(img, caption='Article Image')
+            st.write(f"Title: {i['title']}")
+            st.write(f"Author: {i["author"]}")
+            st.write(f"News: {i['description']}")
+            st.write(f"Link to the article: {i["url"]}")
+            st.write('---------------------------------')
 
-        num = num + 1
+            num = num + 1
+    else:
+        st.error("Please enter a valid news category 😡")
